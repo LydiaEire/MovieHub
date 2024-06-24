bd3be4b0522454cf92a70e5b8d04f7cecb655800
Lydia <nancyeire1987@gmail.com>
2024-06-24 22:30:53 +0300

    updating vite

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import "assets/home.scss";`
+            }
+        }
+    }
+});
+
